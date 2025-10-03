88f5d5690cb1dd939dbbdce2435a39dddc715477
Scott LaVigne <lavignes@amazon.com>
2025-10-03 16:36:31 -0700

    unit test

--- cnopc/tst/tok.c
+++ cnopc/tst/tok.c
@@ -1,3 +1,15 @@
+#include <nop/tok.h>
+
+#include <assert.h>
+
 int main() {
+    Toks ts = {0};
+    toksInitView(&ts, VIEW("test.nop"), VIEW("\
+        pkg Test;                             \
+                                              \
+        use U32;                              \
+    "));
+    assert(toksPeek(&ts) == TOK_PKG);
+
     return 0;
 }
